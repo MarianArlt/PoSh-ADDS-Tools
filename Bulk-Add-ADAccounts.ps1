115e59704ded71ee7af43d353722a8a29733478e
marianarlt <marianarlt@icloud.com>
2023-05-22 11:10:17 +0200

    Parameters Capitalized

--- Bulk-Add-ADAccounts.ps1
+++ Bulk-Add-ADAccounts.ps1
@@ -1,41 +1,41 @@
 [CmdletBinding()]
 param (
     [parameter(mandatory)]
-    [string] $domain,
+    [string] $Domain,
 
     [parameter()]
-    [string] $name = "Test_User",
+    [string] $Names = "Test_User",
 
     [parameter()]
-    [string] $password = "p@ssw0rd",
+    [string] $Password = "p@ssw0rd",
 
     [parameter()]
-    [string] $oupath = "OU_USR.OU_TEST",
+    [string] $OUPath = "OU_USR.OU_TEST",
 
     [parameter()]
-    [int] $create = 10,
+    [int] $Create = 10,
 
     [parameter()]
-    [switch] $computer,
+    [switch] $Computer,
 
     [parameter()]
-    [switch] $csv,
+    [switch] $CSV,
 
     [parameter()]
-    [string] $csvdelimiter = ","
+    [string] $CSVDelimiter = ","
 )
 
-$secret = ConvertTo-SecureString -String $password -AsPlainText -Force
-$ou_directories = $oupath.Split(".").trim()
-$dc_distinguished = ("DC=", ($domain.Split(".").trim() -join ",DC=") -join "")
+$secret = ConvertTo-SecureString -String $Password -AsPlainText -Force
+$ou_directories = $OUPath.Split(".").trim()
+$dc_distinguished = ("DC=", ($Domain.Split(".").trim() -join ",DC=") -join "")
 $ou_distinguished = ("OU=", ($ou_directories -join ",OU=") -join "")
 $full_path = "$ou_distinguished,$dc_distinguished"
 $ou_parents = $dc_distinguished
 $error_counter = 0
 [array]::reverse($ou_directories)
 
-if ($csv) {
-    $user_data = Import-Csv "bulk-add-adaccounts.csv" -Delimiter $csvdelimiter
+if ($CSV) {
+    $user_data = Import-Csv "bulk-add-adaccounts.csv" -Delimiter $CSVDelimiter
 }
 
 for ($i = 0; $i -lt $ou_directories.Count; $i++) {
@@ -54,28 +54,28 @@ for ($i = 0; $i -lt $ou_directories.Count; $i++) {
     }
 }
 
-for ($i = 1; $i -le $create; $i++) {
+for ($i = 1; $i -le $Create; $i++) {
     try {
         $description = "Test Account (Remove for Production)"
-        if ($csv) {
+        if ($CSV) {
             $given = $user_data.first_name | Get-Random
             $surname = $user_data.last_name | Get-Random
             $display = "$given $surname"
             $account = "$given$surname".replace("'", "").tolower()
-            $upn = "$account@$domain"
+            $upn = "$account@$Domain"
             New-ADUser -Name $display -GivenName $given -Surname $surname -DisplayName $display -SamAccountName $account -UserPrincipalName $upn -AccountPassword $secret -Path $full_path -Description $description -Enabled 1 -ChangePasswordAtLogon 0 -ErrorAction Stop
 
-        } elseif ($computer) {
-            $given = ($name, "_", ([string]($i)).padleft(($create.count + 1), "0") -join "")
+        } elseif ($Computer) {
+            $given = ($Names, "_", ([string]($i)).padleft(($Create.count + 1), "0") -join "")
             New-ADComputer -Name $given -AccountPassword $secret -Path $full_path -Description $description -Enabled 1 -ChangePasswordAtLogon 0 -ErrorAction Stop
 
         } else {
-            $given = ($name, "_", ([string]($i)).padleft(($create.count + 1), "0") -join "")
+            $given = ($Names, "_", ([string]($i)).padleft(($Create.count + 1), "0") -join "")
             New-ADUser -Name $given -AccountPassword $secret -Path $full_path -Description $description -Enabled 1 -ChangePasswordAtLogon 0 -ErrorAction Stop
         }
     }
     catch {
-        if ($csv) {
+        if ($CSV) {
             Write-Output ("[$display]: ", $_.Exception.Message -join "")
         } else {
             Write-Output ("[$given]: ", $_.Exception.Message -join "")
@@ -97,26 +97,26 @@ Takes several optional arguments or sane defaults to create a bulk of randomized
 accounts to use in active directory test environments.
 CSV data can be used for creation. The provided CSV contains purely fictional data.
 
-.PARAMETER domain
+.PARAMETER Domain
 
 Fully qualified domain name of the domain where this user shall be created
 
-.PARAMETER username
+.PARAMETER Names
 
 Only used when not reading from CSV. Creates users based on this string.
 
-.PARAMETER password
+.PARAMETER Password
 
 Change the password for all users.
 [Default]: "p@ssw0rd"
 
-.PARAMETER oupath
+.PARAMETER OUPath
 
 Change the pathing for the organizational unit structure of this bulk.
 Write from child to parent and separate with dots.
 [Default]: "OU_USR.OU_TEST"
 
-.PARAMETER create
+.PARAMETER Create
 
 The amount of accounts to be created.
 [Default]: 10
@@ -125,19 +125,19 @@ The amount of accounts to be created.
 
 Create computer objects instead of users.
 
-.PARAMETER csv
+.PARAMETER CSV
 
 Read data from CSV file specified. Path absolute or relative to script.
 The file MUST have a column labeled first_name and a column labeled last_name.
 If your CSV file does not use a comma as delimiter, use the corresponding parameter.
 
-.PARAMETER csvdelimiter
+.PARAMETER CSVDelimiter
 
 If your CSV file uses a non-standard delimiter use this in conjunction with -csv
 
 .EXAMPLE
 
-Add-Bulk-ADUsers -domain contoso.com -create 40 -oupath "Test_Users.Accounting._NYC" -csv
+Add-Bulk-ADUsers -Domain contoso.com -Create 40 -OUPath "Test_Users.Accounting._NYC" -CSV
 
 .LINK
 
